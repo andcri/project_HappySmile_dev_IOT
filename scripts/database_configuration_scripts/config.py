62507b243019b22884bd7f8fa10dec18723ce5a7
andcri <52462858+andcri@users.noreply.github.com>
2019-12-11 15:21:09 +0200

    Update config.py

--- scripts/database_configuration_scripts/config.py
+++ scripts/database_configuration_scripts/config.py
@@ -1,6 +1,6 @@
 """
 Configuration info of the postgre database
 """
-DATABASE_URI = 'postgres://yqjmwpmnlrrehd:55176ce134225cfd8885a8dac7402fd383cae44009905d6fdda3843b80105dda@ec2-54-75-224-168.eu-west-1.compute.amazonaws.com:5432/dmbn9tt9l3mvd'
+DATABASE_URI = 'postgres://yq*****hd:551*****80105dda@ec2-****naws.com:5432/dmb****vd'
 
-DATABASE_URI_LOCALE = 'postgres+psycopg2://pi:Mypassword@localhost:5432/devhappysmile'
+DATABASE_URI_LOCALE = 'postgres+psycopg2://pi:M****d@localhost:5432/dev****ysmile'
